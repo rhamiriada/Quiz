7e5e42e36fbb7198fd240a356553f44cdf494b9b
rhamiriada <rherrera@eresmas.com>
2015-07-12 10:31:12 +0200

    Cambios página 24: -- Añadir favicon -- Quitar routes ---- users.js ---- modificar app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 var partials = require('express-partials');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// Modificación página 24
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -17,7 +18,8 @@ app.set('view engine', 'ejs');
 app.use(partials());
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// Modificación página 24. Añado favicon
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +27,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+// Modificación página 24
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
