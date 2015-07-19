9d55cef93e9236f761bd1f106c867bddd189bd86
rhamiriada <rherrera@eresmas.com>
2015-07-19 10:49:57 +0200

    Con autolad

--- models/models.js
+++ models/models.js
@@ -19,7 +19,9 @@ sequelize.sync().then(function() {
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.bulkCreate( 
-        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
+        [
+          {pregunta: 'Capital de España', respuesta: 'Madrid'},
+          {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
           {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
         ]
       ).then(function(){console.log('Base de datos inicializada')});
