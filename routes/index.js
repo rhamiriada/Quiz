8551bd14b17a67ab3b31d693ea6eff7c82179f6c
rhamiriada <rherrera@eresmas.com>
2015-08-04 19:47:23 +0200

    Autorizacion

--- routes/index.js
+++ routes/index.js
@@ -23,15 +23,15 @@ router.get('/quizes',   quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',         quizController.answer);
 
-router.get('/quizes/:quizId(\\d+)/edit',             quizController.edit);
-router.put('/quizes/:quizId(\\d+)',                   quizController.update);
-router.delete('/quizes/:quizId(\\d+)',               quizController.destroy);
+router.get('/quizes/:quizId(\\d+)/edit',      sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',            sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.destroy);
 
 router.get('/quizes/author', function(req, res) {	res.render('author');});
 router.get('/quizes/dedicatoria', function(req, res) {	res.render('dedicatoria');});
 
-router.get('/quizes/new',                     quizController.new);
-router.post('/quizes/create',                quizController.create);
+router.get('/quizes/new',                       sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',                  sessionController.loginRequired, quizController.create);
 
 router.get('/quizes/:quizId(\\d+)/comments/new',         commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
